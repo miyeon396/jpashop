071fe7ce311a0dea9e333bf4f489a0015c6a60af
miyeon396 <miyeon396@naver.com>
2024-04-10 18:12:35 +0900

    주문 도메인 기능 구현 및 테스트
    - 가장 중요한 비지니스 로직이 얽혀서 돌아가는 주문 도메인
    - 테스트 안됨 수정 필요

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable //어딘가에 내장이 될 수 있따.
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipCode;
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,11 +2,14 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -23,4 +26,35 @@ public class OrderItem {
 
     private int orderPrice; //주문 당시 가격
     private int count;
+
+    //생성 메서드
+//    protected OrderItem() {
+//        //쓰지 말라는거 아래 createOrderItem만 쓰라는거
+//        //jpa에서 사용해야하니 private는 안되고 protected.
+//        //jpa에서 protected는 쓰지 말란거
+//        // 더 간단하게는 위에 롬복으로 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+//    }
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비지니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
