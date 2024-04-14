6ce2e2c00f3ffed2c61f0dc8749b22bb8e2c3cc9
miyeon396 <miyeon396@naver.com>
2024-04-14 18:24:38 +0900

    활용2 : 컬렉션 최적화 v6 dto로 반환 쿼리 한번으로
    - 쿼리는 한번인데 좋은건 잘 모르게씀
    - 페이징이 안됨
    - 조인으로 인해서 DB에서 애플리케이션에 전달하는 데이터에 중복 데이터가 추가되므로 상황에 따라 v5보다 더 느릴 수 있다.
    - 애플리케이션에서 추가 작업이 크다.

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -1,16 +1,15 @@
 package jpabook.jpashop.repository.order.query;
 
-import jpabook.jpashop.api.OrderApiController;
 import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     private Long orderId;
@@ -29,4 +28,12 @@ public class OrderQueryDto {
 //        this.orderItems = orderItems;
     }
 
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
 }
