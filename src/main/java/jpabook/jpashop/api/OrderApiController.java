5e4a7d7665516343a72b7865cb0ea5b55bb77278
miyeon396 <miyeon396@naver.com>
2024-04-14 16:07:13 +0900

    활용2 : 컬렉션 최적화 v1, v2 엔티티 반환, DTO반환
    - DTO반환 시 하위의 데이터까지도 Enitity에서 Dto로 변환 반환해야함
    - 그러나 쿼리가 무수히많이 동작한다는 이슈가 존재한다.

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,86 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 초기화
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+
+        return all;
+
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .toList();
+        return collect;
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); //이게 없으니 안나옴 엔티티라. 프록시 초기화 -> 그러나 일케 하면 안되고 얘 조차도 DTO만들어야해서 주석
+            this.orderItems = order.getOrderItems()
+                    .stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .toList();
+        }
+
+    }
+
+    @Getter
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getItem().getPrice();
+            this.count = orderItem.getCount();
+        }
+
+    }
+}
